fd02865aadab04289897a8decee13521630dc2fa
Nircek <nircek-2103@protonmail.com>
2019-02-14 16:38:37 +0100

    Add connecting to PPS

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 import configparser
+from pypps.PyPPSPS import *
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-print(config['PseudoPHPServer']['url'], config['PseudoPHPServer']['token'])
+p = PyPPSPS(config['PseudoPHPServer']['url'], config['PseudoPHPServer']['token'])
+p.connect()
+print(p.version())
+def q(x):
+    print(x)
+    return x
+try:
+    p.varwrite('', 'i', str(q(int(p.varread('', 'i')[0])+1)))
+except PPSNullError:
+    p.varwrite('', 'i', '0')
