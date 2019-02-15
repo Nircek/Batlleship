90f51efe4750b847afd658ff5d45ec42612e3fa3
Nircek <nircek-2103@protonmail.com>
2019-02-15 19:45:27 +0100

    Add basic init of Server Update PyPPS

--- server.py
+++ server.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python3
 import configparser
 from pypps.PyPPSPS import *
+import json
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-p = PyPPSPS(config['PseudoPHPServer']['url'], config['PseudoPHPServer']['token'])
-p.connect()
-print(p.version())
 def q(x):
     print(x)
     return x
-try:
-    p.varwrite('', 'i', str(q(int(p.varread('', 'i')[0])+1)))
-except PPSNullError:
-    p.varwrite('', 'i', '0')
+
+class Server:
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        self.p = PyPPSPS(config['PseudoPHPServer']['url'], config['PseudoPHPServer']['token'])
+        self.p.connect()
+        print(self.p.version())
+        self.initVars()
+
+    def initVars(self):
+        self.rooms = []
+        self.getRooms()
+
+    def count(self):
+        try:
+            self.p.varwrite('', 'i', str(q(int(self.p.varread('', 'i')[0])+1)))
+        except PPSNullError:
+            self.p.varwrite('', 'i', str(q(0)))
+
+    def getRooms(self):
+        try:
+            rooms = json.loads(''.join(self.p.varread('', 'rooms')))
+        except PPSNullError:
+            self.setRooms()
+
+    def setRooms(self):
+        self.p.varwrite('', 'rooms', json.dumps(self.rooms))
+
+s = Server()
+s.count()
