40737136384bb7eeb3cd7fb093c26a11a6b1a175
Nircek <nircek-2103@protonmail.com>
2019-02-16 15:35:34 +0100

    Update PyPPS

--- server.py
+++ server.py
@@ -22,28 +22,28 @@ class Server:
 
     def count(self):
         try:
-            self.p.varwrite('', 'i', str(q(int(self.p.varread('', 'i')[0])+1)))
+            self.p.varwrite('i', str(q(int(self.p.varread('i')[0])+1)))
         except PPSNullError:
-            self.p.varwrite('', 'i', str(q(0)))
+            self.p.varwrite('i', str(q(0)))
 
     def getRooms(self):
         try:
-            rooms = json.loads(''.join(self.p.varread('', 'rooms')))
+            rooms = self.p.varreadj('rooms')
         except PPSNullError:
             self.setRooms()
 
     def setRooms(self):
-        self.p.varwrite('', 'rooms', json.dumps(self.rooms))
+        self.p.varwritej('rooms', self.rooms)
 
     def handleEvent(self):
         try:
-            ev = self.p.pop()
+            ev = self.p.popj()
         except PPSNullError:
             return False
         print(ev)
-        if len(ev) > 1:
-            if ev[1] == 'getRooms()':
-                self.p.reply(ev[0], json.dumps({'cmd': 'setRooms()', 'data': json.dumps(self.rooms)}))
+        if 'cmd' in ev:
+            if ev['cmd'] == 'getRooms()':
+                self.p.replyj(ev['user'], {'cmd': 'setRooms()', 'data': self.rooms})
         return True
 
 s = Server()
