889dba292698712b711f717f82dbcedd8a2ded98
Nircek <nircek-2103@protonmail.com>
2019-02-21 20:51:21 +0100

    Add asking for server's URL Repair threading and tkinter stuff

--- client.py
+++ client.py
@@ -7,22 +7,27 @@ from tkinter import simpledialog, messagebox
 from threading import Timer
 
 def get_credentials():
-    # SRC: https://gist.github.com/Nircek/4d32a447a6783a4e63ee3cf6dbb1afb7
+    # inspired by https://gist.github.com/Nircek/4d32a447a6783a4e63ee3cf6dbb1afb7
     creds = []
     tk = Tk()
     tk.title('Log in')
-    Label(tk, text='Username: ').grid(column=0, row=0, sticky=W)
-    Label(tk, text='Password: ').grid(column=0, row=1, sticky=W)
+    Label(tk, text='Server URL: ').grid(column=0, row=0, sticky=W)
+    Label(tk, text='Username: ').grid(column=0, row=1, sticky=W)
+    Label(tk, text='Password: ').grid(column=0, row=2, sticky=W)
     u = Entry(tk)
     u.focus_set()
     u.grid(column=1, row=0, pady=2)
+    n = Entry(tk)
+    n.grid(column=1, row=1, pady=2)
+    u.bind('<Return>', lambda x:n.focus_set())
     p = Entry(tk, show='*')
-    p.grid(column=1, row=1, pady=2)
-    u.bind('<Return>', lambda x:p.focus_set())
-    b = Button(tk, text='Log in!', command=lambda:(lambda x:tk.destroy())(creds.extend([u.get(), p.get()])))
-    b.grid(column=0, row=2, columnspan=2, pady=5)
+    p.grid(column=1, row=2, pady=2)
+    n.bind('<Return>', lambda x:p.focus_set())
+    b = Button(tk, text='Log in!', command=lambda:(lambda x:tk.quit())(creds.extend([u.get(), n.get(), p.get()])))
+    b.grid(column=0, row=3, columnspan=2, pady=5)
     p.bind('<Return>', lambda x: b.invoke())
     tk.mainloop()
+    tk.destroy()
     return creds if creds else None
 
 class FrameHolder(Tk):
@@ -42,18 +47,21 @@ class Main(Frame):
     def __init__(self, master, client):
         Frame.__init__(self, master)
         self.client = client
-        Button(self, text='Log in', command = lambda: (lambda x: client.p.login(x[0], x[1]) if x is not None else None)(get_credentials())).pack()
-        # TODO: ask about url
-        # TODO: client.p can be None because of initialising PPSPC after 'Connect' button clickin
         Button(self, text='Connect', command=self.client.makePPSPC).pack()
-        Button(self, text='Quit', command=self.master.destroy).pack()
+        Button(self, text='Quit', command=self.client.on_quit).pack()
 
 class BattleshipClient:
     def __init__(self):
         self.frames = FrameHolder(lambda x:Main(x,self))
+        self.frames.protocol("WM_DELETE_WINDOW", self.on_quit)
         self.p = None
-        self.interval_set = False
         self.connected = False
+        self.t = None
+
+    def on_quit(self):
+        print('on_quit')
+        self.interval = -1
+        self.frames.destroy()
 
     @property
     def interval(self):
@@ -61,33 +69,31 @@ class BattleshipClient:
 
     @interval.setter
     def interval(self, x):
+        if self.t is not None:
+            self.t.cancel()
         self.__interval = x
-        if self.interval >= 0 and not self.interval_set:
-            print(Timer(self.interval, self.intervalFunc).start())
+        if self.interval >= 0 and (self.t is None or not self.t.is_alive()):
+            self.t = Timer(self.interval, self.intervalFunc)
+            self.t.start()
 
     def intervalFunc(self):
-        if self.interval < 0:
-            self.interval_set = False
-            return
-        self.interval_set = True
-        # ---
         ev = self.p.refreshj()
         print(ev)
-        if ev['cmd'] == 'error()':
-            messagebox.showerror(ev['type']+' error', str(ev['data']))
-        # ---
         if self.interval >= 0:
-            Timer(self.interval, self.intervalFunc).start()
-        else:
-            self.interval_set = False
+            if ev['cmd'] == 'error()':
+                messagebox.showerror(ev['type']+' error', str(ev['data']))
+                self.t = Timer(self.interval, self.intervalFunc)
+                self.t.start()
 
     def makePPSPC(self):
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        self.p = PyPPSPC(config['PseudoPHPServer']['url'])
-        self.p.input = lambda x,y:simpledialog.askstring(x, y, parent=master)
-        self.p.bool = lambda x,y:messagebox.askokcancel(x, y)
+        url, username, password = get_credentials()
+        self.p = PyPPSPC(url)
+        self.p.input = lambda x,y:simpledialog.askstring(x, y, parent=self.frames)
+        self.p.bool = lambda x,y:messagebox.askokcancel(x, y, parent=self.frames)
+        print('Connecting... ', end='')
         self.p.connect()
+        print('done.')
+        self.p.login(username, password)
         self.connected = True
         self.interval = 5
 
